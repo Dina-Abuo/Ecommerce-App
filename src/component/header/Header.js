74277a0e763a46ce26d6561721ebc8a00feea57c
Dina-Abuo <dynaalkhfajy73@gmail.com>
2023-03-07 00:57:30 +0200

    Work on me header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import "./style.css"
+import { Link } from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+const logo = (
+    <div className='logo'>
+        <Link to="/" className='link'>
+            <h2>
+                e<span>Shop</span>.
+            </h2>
+        </Link>
+    </div>
+)
+
+const cart = (
+    <span className='cart'>
+        <Link className='linkCart' to='/car'>
+            Cart
+            <FaShoppingCart style={{ fontSize: '20PX' }} />
+        </Link>
+        <span >0</span>
+    </span>
+)
 
 const Header = () => {
+
+
     return (
         <div>
-            Header
+            <header className='header'>
+                {logo}
+                <nav>
+                    <ul >
+                        <li><Link className='link' to="/"> Home</Link></li>
+                        <li><Link className='link' to="/contact"> Contact Us</Link></li>
+                    </ul>
+                </nav>
+                <div className='hearder-right'>
+                    <span className='Links'>
+                        <Link className='link' to="/login">  Login</Link>
+                        <Link className='link' to="/register">Register</Link>
+                        <Link className='link' to="/order-history">  My Orders</Link>
+                    </span>
+                    {cart}
+                </div>
+                <div className='menuIcone'>
+                    <AiOutlineMenu style={{ fontSize: '28PX' }} />
+                    {cart}
+                </div>
+            </header>
+
         </div>
     )
 }
